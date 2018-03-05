7ca846a50af5a6a34f6a91a584b005251fd5c77a
EOTMB <agustinmorelli88@gmail.com>
2018-03-04 23:40:58 -0300

    Banco Simulacion Banco actualizado

--- banco.py
+++ banco.py
@@ -1,5 +1,3 @@
-from random import randint
-
 class Fila(object):
     """Clase base de fila"""
 
@@ -10,12 +8,9 @@ class Fila(object):
 
 class FilaPreferencial(Fila):
     """Clase de la fila de los clientes preferenciales"""
-    def __init__(self):
-        super(FilaPreferencial,self).__init__()
-        self.maxenfila = 15
     def insertar(self, cliente):
         """Inserta un nuevo cliente en la fila preferencial"""
-        if cliente.categoria == 'pref':
+        if cliente.categoria == 'preferencial':
             self.enfila += 1
             self.fila.append(cliente)
         else:
@@ -25,9 +20,9 @@ class FilaPreferencial(Fila):
         self.enfila -= 1
         self.fila.pop(0)
 
-    def abrircajanueva(self,filanueva):
+    def abrircajanueva(self,maxenfila,filanueva):
         """Si maxenfila es menor que la cantidad de clientes actualmente en espera, abro nueva caja"""
-        if self.enfila > self.maxenfila:
+        if self.enfila > maxenfila:
 			filanueva = FilaPreferencial()
 			filanueva.enfila = self.enfila/2
 			filanueva.fila = self.fila[:filanueva.enfila]
@@ -61,66 +56,3 @@ class cliente(object):
      def modificarcategoria(self, categoria):
         """modifica el atributo categoria del cliente """
         self.categoria = categoria
-
-
-if __name__ == "__main__":
-    """ simular una fila en una entidad bancaria"""
-    sinAtender=[]
-    filaGeneral=FilaGeneral()
-    filaPreferencial1=FilaPreferencial()
-    filaPreferencial2=FilaPreferencial()
-    filaPreferencial3=FilaPreferencial()
-    i=0
-    j=0
-    for i in xrange(100):
-        dni = randint(0,40000000)
-        cli = cliente(dni)
-        if i > 60:
-            cli.modificarcategoria('pref')
-        sinAtender.append(cli)
-
-    for client in sinAtender:
-        largoFila1 = filaPreferencial1.enfila
-        largoFila2 = filaPreferencial2.enfila
-        largoFila3 = filaPreferencial3.enfila
-        if largoFila1  == 0 and largoFila2 == 0 and largoFila3 == 0:
-            filaPreferencial1.insertar(client)
-        elif largoFila2 < largoFila1 and largoFila2 < largoFila3:
-            filaPreferencial2.insertar(client)
-        elif largoFila3 < largoFila1:
-            filaPreferencial3.insertar(client)
-        else:
-            filaPreferencial1.insertar(client)
-
-        if client.categoria != 'pref':
-            filaGeneral.insertar(client)
-            print 'El cliente DNI' + string(client.dni) + 'se agrego a la fila general'
-        else:
-            print 'El cliente DNI' + string(client.dni) + 'se agrego a la fila preferencial'
-        filasPreferenciales=[filaPreferencial1,filaPreferencial2,filaPreferencial3]
-
-        for i in range(filasPreferenciales):
-            fila = filasPreferenciales[i]
-            filanueva = filasPreferenciales[i+1]
-            largoFila = fila.enfila
-            largofilanueva = filanueva.enfila
-            longitudmaxima = fila.maxenfila
-            if largoFila >= longitudmaxima:
-                fila.abrircajanueva(filanueva)
-                print 'Se abrio una nueva caja para clientes preferenciales'
-
-        if j == 2:
-            for fila in filasPreferenciales:
-                posicionCliente = fila.fila[0]
-                clienteAtendido = string(posicionCliente.dni)
-                print 'Se atendio el cliente'+ clienteAtendido + 'en la fila preferencial'
-                fila.atender()
-            j = 0
-        if i == 3:
-            posicionClienteGeneral = filaGeneral.fila[0]
-            clienteGeneralAtendido = string(posicionClienteGeneral.dni)
-            print 'Se atendio el cliente'+ clienteGeneralAtendido + 'en la fila preferencial'
-            filaGeneral.atender()
-
-        i += 1
-        j += 1
